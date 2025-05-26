a9f53720b8400c679d7b27e91098d62671cff0d4
GrantC123 <grantcrowderdesign@gmail.com>
2025-05-26 00:26:38 -0400

    ok

--- wc-breadcrumbs.js
+++ wc-breadcrumbs.js
@@ -14,16 +14,31 @@ if (typeof HTMLElement !== 'undefined') {
       
         render() {
             const projectName = this.getAttribute('project-name') || '';
+            const color = this.getAttribute('color') || 'default';
+            
+            // Define color classes based on the color attribute
+            let linkClasses, separatorClasses, currentPageClasses;
+            
+            if (color === 'white') {
+                linkClasses = 'no-underline text-white/70 hover:underline transition-colors duration-300';
+                separatorClasses = 'mx-2 text-gray-600';
+                currentPageClasses = 'text-purple-200 font-medium';
+            } else {
+                // Default color scheme
+                linkClasses = 'no-underline text-gray-500 hover:text-purple-600 transition-colors duration-300';
+                separatorClasses = 'mx-2 text-gray-200';
+                currentPageClasses = 'text-gray-900';
+            }
             
             this.innerHTML = `
                 <nav class="mx-auto px-8 py-4">
                     <div class="">
                         <ol class="list-none p-0 m-0 flex items-center mx-auto max-w-7xl">
-                            <li><a href="index.html" class="no-underline text-gray-500 hover:text-purple-600 transition-colors duration-300">Home</a></li>
-                            <li class="mx-2 text-gray-200">/</li>
-                            <li><a href="index.html#project-section" class="no-underline text-gray-500 hover:text-purple-600 transition-colors duration-300">Work</a></li>
-                            <li class="mx-2 text-gray-200">/</li>
-                            <li><span class="text-gray-900">${projectName}</span></li>
+                            <li><a href="index.html" class="${linkClasses}">Home</a></li>
+                            <li class="${separatorClasses}">/</li>
+                            <li><a href="index.html#project-section" class="${linkClasses}">Work</a></li>
+                            <li class="${separatorClasses}">/</li>
+                            <li><span class="${currentPageClasses}">${projectName}</span></li>
                         </ol>
                     </div>
                 </nav>
