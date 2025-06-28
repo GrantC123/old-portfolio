2428460c6aa6d61c9f9f8ed1d4c7e5afefd0bfaf
GrantC123 <grantcrowderdesign@gmail.com>
2025-06-27 22:50:36 -0400

    Update wc-footer.js

--- wc-footer.js
+++ wc-footer.js
@@ -8,7 +8,7 @@ class MyFooter extends HTMLElement {
     render() {
         const currentYear = new Date().getFullYear();
         this.innerHTML = `
-            <footer class="bg-purple-900 text-white font-inter">
+            <footer class="bg-coral-900 text-white font-inter">
                 <div class="text-center py-8 border-t border-gray-700 px-8 flex justify-between max-w-7xl mx-auto">
                     <span class="text-sm text-white text-shadow-sm">grantcrowderdesign@gmail.com</span>
                     <p class="text-sm text-white text-shadow-sm">&copy; ${currentYear} Grant Crowder Design | Crafted with love <img src="Images/heart.svg" alt="heart" class="w-5 h-5 inline-block drop-shadow-sm drop-shadow-pink-400/50"></p>
